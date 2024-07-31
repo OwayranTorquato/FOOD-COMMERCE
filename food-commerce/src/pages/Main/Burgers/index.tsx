2337d40cb198c0c7973af61b7a61f608afa3704a
OwayranTorquato <owayran1998@gmail.com>
2024-07-31 02:01:05 -0300

    Lista de dados dos itens (Burguers)

--- food-commerce/src/pages/Main/Burgers/index.tsx
+++ food-commerce/src/pages/Main/Burgers/index.tsx
@@ -2,9 +2,32 @@ import { Head } from '../../../components/Head'
 import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Burgers() {
+  //const data = lista de dados dos hambúrgueres
+  const data = [
+    {
+      id: 1,
+      snack: 'burguer',
+      name: 'Mega',
+      description:
+        'O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burguer',
+      name: 'Extra Bacon',
+      description:
+        'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado, queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ]
+
+  console.log('data', data)
   return (
     <>
-      <Head title='Hambúrgueres'/>
+      <Head title='Hambúrgueres' />
       <SnackTitle>Hambúrgueres</SnackTitle>
     </>
   )
