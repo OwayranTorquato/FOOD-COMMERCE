6d5ff1905207dc818e480c1bc22c73701f98aa1c
OwayranTorquato <owayran1998@gmail.com>
2024-07-31 01:17:20 -0300

    modificando title das pages com SnackTitle

--- food-commerce/src/pages/Main/Drinks/index.tsx
+++ food-commerce/src/pages/Main/Drinks/index.tsx
@@ -1,5 +1,11 @@
-import { Head } from "../../../components/Head"
+import { Head } from '../../../components/Head'
+import { SnackTitle } from '../../../components/SnackTitle'
 
 export default function Drinks() {
-  return <Head title='Drinks' description="Nossos melhores drinks"/>
+  return (
+    <>
+      <Head title='Bebidas'/>
+      <SnackTitle>Bebidas</SnackTitle>
+    </>
+  )
 }
