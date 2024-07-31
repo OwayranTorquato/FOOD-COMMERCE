24793cf74d805de14372f9f99511e079e54f5342
OwayranTorquato <owayran1998@gmail.com>
2024-07-31 03:44:57 -0300

    Criando as funções de API iniciais

--- food-commerce/src/services/api.ts
+++ food-commerce/src/services/api.ts
@@ -4,5 +4,15 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
+//https://seusite.com.br/api/burgers
+//https://seusite.com.br/api/pizzas
+//https://seusite.com.br/api/drinks
+//https://seusite.com.br/api/ice-creams
+//Captura das informações nos endpoints
+
+export const getBurgers = () => api.get('/burgers')
+export const getPizzas = () => api.get('/pizzas')
+export const getDrinks = () => api.get('/drinks')
+export const getIceCreams = () => api.get('/ice-creams')
 
 export default api
